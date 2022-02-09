05cac520c14e2e62b6f77c89815d76f23802952e
Dmitry Neverovski <dmitryneverovski@gmail.com>
2022-02-09 21:51:52 +0300

    feat: user test

---
+++ tests/e2e/modules/user.test.ts
@@ -0,0 +1,26 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+
+import { HttpStatus, CodeResponse, HttpExceptionType } from '@utils/index';
+
+import { router } from '../lib';
+
+chai.use(chaiHttp);
+
+describe('User API endpoint', () => {
+  it('should get 401 without access token', (done) => {
+    void chai
+      .request('localhost:5858')
+      .get(router.users.getCurrent)
+      .end((_err, res) => {
+        const { message, code, status } =
+          CodeResponse[HttpExceptionType.TOKEN_NOT_PROVIDED];
+
+        expect(res.status).to.equal(HttpStatus.Unauthorized);
+        expect(res.body).to.have.property('message', message);
+        expect(res.body).to.have.property('code', code);
+        expect(res.body).to.have.property('status', status);
+        done();
+      });
+  });
+});
